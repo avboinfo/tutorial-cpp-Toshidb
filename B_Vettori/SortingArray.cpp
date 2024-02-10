939effb2b8065e0e102f04c9aac994111073c960
Toshidb <153986600+Toshidb@users.noreply.github.com>
2024-02-10 11:38:02 +0000

    Mattei Christian

--- B_Vettori/SortingArray.cpp
+++ B_Vettori/SortingArray.cpp
@@ -7,57 +7,88 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
-
+using the std namespace;
 const string FILENAME = "SortingArray.txt";
+int numberExchanges = 0;
 
-void stampaVettore( string v[], int l ) {
-    cout << endl;
-    for (int i=0; i<l; i++) cout << v[i] << "\n";
-    cout << endl;
+void printVector( string v[], int l ) {
+cout<<endl;
+for (int i=0; i<l; i++) cout << v[i] << "\n";
+cout<<endl;
 }
 
 int bubbleSort( string v[], int l ) {
-    int numOp = 0;
-    string tmp;
-    for (int i=0; i<l; i++ ) {
-        bool alreadySorted = true;
-        for (int j=0; j<l-1; j++ ) {
-            if ( v[j] > v[j+1] ) {
-                alreadySorted = false;
-                tmp = v[j];
-                v[j] = v[j+1];
-                v[j+1] = tmp;
-            }
-            numOp++;
-        }
-        if (alreadySorted) break;
-    }
-    return numOp;
+int numOp = 0;
+string tmp;
+for (int i=0; i<l; i++ ) {
+bool alreadyOrdered = true;
+for (int j=0; j<l-1; j++ ) {
+if ( v[j] > v[j+1] ) {
+alreadyOrdered = false;
+tmp = v[j];
+v[j] = v[j+1];
+v[j+1] = tmp;
+}
+numOp++;
+}
+if (already sorted) break;
+}
+return numOp;
+}
+void swap(string arr[] , int pos1, int pos2){
+string temperature;
+temp = arr[pos1];
+arr[pos1] = arr[pos2];
+arr[pos2] = temperature;
 }
 
+int partition(string arr[], int low, int high, string pivot){
+int i = low;
+int j = low;
+while( i <= tall){
+if(arr[i] > pin){
+i++;
+}
+other{
+swap(arr,i,j);
+i++;
+j++;
+}
+}
+return j-1;
+}
+
+void quickSort(string arr[], int low, int high){
+if(low <high){
+string pin = arr[high];
+int pos = partition(arr, bottom, top, pivot);
+
+QuickSort(arr, low, pos-1);
+QuickSort(arr, pos+1, up);
+}
+}
 int main()
 {
-    ifstream f;
-    f.open(FILENAME);
-    if ( f.fail() ) { cout << "Il file di input non esiste"; return -1; }
-    
-    // legge tutto il file per misurarne il numero di righe
-    int n;
-    string s;
-    for (n=0; getline(f,s); n++);
-    f.close();
-    cout << "il file di input contiene " << n << " righe" << endl;
-    
-    // rilegge il file per caricarne le righe in un vettore
-    string vs[n];
-    f.open(FILENAME);
-    for (int i=0; i<n; i++) getline( f, vs[i] );
+ifstream f;
+f.open(FILENAME);
+if ( f.fail() ) { cout << "The input file does not exist"; return -1; }
 
-    stampaVettore(vs, n);
-    int x = bubbleSort(vs, n);
-    cout << "Eseguiti " << x << " confronti." << endl;
-    stampaVettore(vs, n);
+integer n;
+strings;
+for (n=0; getline(f,s); n++);
+f.close();
+cout << "input file contains " << n << " lines" << endl;
 
-    return 0;
+string vs[n];
+f.open(FILENAME);
+for (int i=0; i<n; i++) getline( f, vs[i] );
+
+printVector(vs, n);
+numberExchanges=0;
+//quick sort(vs, 0, n-1);
+numberScambi=bubbleSort(vs, n);
+cout << "Executed " << numeroScambi << " exchanges." <<end;
+printVector(vs, n);
+
+return 0;
 }
